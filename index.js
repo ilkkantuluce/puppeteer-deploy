8d171cb2dc0237aa67bb0054d888c640b4250e22
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 16:01:00 +0100

    fix

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 
-app.get("/", async (req, res) => {
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/", async (req, res) => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto("https://www.youtube.com/@FREDOPG");
+  await page.goto("https://www.youtube.com/@test");
   await page.waitForTimeout(4000)
   const text = await page.$eval("#subscriber-count", (el) => el.innerHTML);
   //await page.screenshot({ path: 'fullpage.png', fullPage: true });
 
-
-  await page.goto("https://www.instagram.com/ebo");
-  await page.waitForTimeout(4000)
-  const text2 = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.xnz67gz.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.xh8yej3.x1gryazu.x10o80wk.x14k21rp.x1porb0y.x17snn68.x6osk4m > section > main > div > header > section > ul > li:nth-child(2) > a > div", (el) => el.innerHTML);
-  
   res.send(text);
-  res.send(text2);
 
   await browser.close();
 });
