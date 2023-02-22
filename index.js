ee75c390d45a9d0b88270d03470328f6e8688374
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 21:27:51 +0100

    fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post("/instagram", async (req, res) => {
   const page = await browser.newPage();
   await page.goto(myText);
   await page.waitForTimeout(4000)
-  const text = await page.$eval("div.xh8yej3.x1gryazu.x10o80wk.x14k21rp.x1porb0y.x17snn68.x6osk4m > section > main > div > header > section > ul > li:nth-child(2) > div > span", (el) => el.innerHTML);
+  const text = await page.$eval("body", (el) => el.innerHTML);
   res.send(text);
   await browser.close();
 });
@@ -50,6 +50,23 @@ app.post("/tiktok", async (req, res) => {
   res.send(text);
   await browser.close();
 });
+
+
+app.post("/twitter", async (req, res) => {
+  const myText = req.body.twitter;
+  //console.log(myText);
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto(myText);
+  await page.waitForTimeout(4000)
+  const text = await page.$eval("div.css-1dbjc4n.r-kemksi.r-1kqtdi0.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-13awgt0.r-18u37iz.r-1w6e6rj > div:nth-child(2) > a > span.css-901oao.css-16my406.r-1nao33i.r-poiln3.r-1b43r93.r-b88u0q.r-1cwl3u0.r-bcqeeo.r-qvutc0 > span", (el) => el.innerHTML);
+  res.send(text);
+  await browser.close();
+});
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("starting....");
