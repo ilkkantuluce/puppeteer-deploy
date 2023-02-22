19a73191c14eea424049f0482bdf2b46ec1ed970
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 10:17:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
+
+
   const browser = await puppeteer.launch({
-    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
     headless:false, 
     defaultViewport:null,
     devtools: true,
     //args: ['--window-size=1920,1170','--window-position=0,0']
     args: ["--no-sandbox", "--disable-setuid-sandbox", "--window-size=1920,1080", "--window-position=1921,0"]
   });
+
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
   await page.goto("https://instagram.com/gio/");
   await page.waitForTimeout(2000)
   const text = await page.$eval("body", (el) => el.innerHTML);
