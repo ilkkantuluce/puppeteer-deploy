d8afb7a8808adab918a5817db00cbc3e638cc918
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 02:45:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", async (req, res) => {
   });
   const page = await browser.newPage();
   await page.goto("https://www.instagram.com/gio/");
-  const text = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.xnz67gz.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.xh8yej3.x1gryazu.x10o80wk.x14k21rp.x1porb0y.x17snn68.x6osk4m > section > main > div > header > section > ul > li:nth-child(2) > a > div > span > span", (el) => el.textContent);
+  const text = await page.$eval("body", (el) => el.textContent);
   res.send(text);
 
   await browser.close();
