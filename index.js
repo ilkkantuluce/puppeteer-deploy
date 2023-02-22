6dee2a33ff0fbeca1254a4cc40fb9a616c1587dd
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 18:55:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
-const app = express();
 
+const app = express();
 app.use(express.urlencoded());
+app.use(express.json())
 
 app.get("/", (req, res) => {
   //res.sendFile(__dirname + "/public/index.html");
