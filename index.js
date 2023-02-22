9d4898f432277acb1d93bec553d164e70cb5c30f
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 10:07:04 +0100

    fix

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+    headless:false, 
+    defaultViewport:null,
+    devtools: true,
+    //args: ['--window-size=1920,1170','--window-position=0,0']
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--window-size=1920,1080", "--window-position=1921,0"]
   });
   const page = await browser.newPage();
   await page.goto("https://instagram.com/gio/");
