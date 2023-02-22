49c503c424c11b4c70f645a4e1733b7484f89d17
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 13:39:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ app.get("/", async (req, res) => {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto("https://livecounts.io/instagram-live-follower-counter/gio");
+  await page.goto("https://famoid.com/instagram-follower-count-checker/");
   await page.waitForTimeout(2000)
-  const text = await page.$eval(".odometer.odometer-auto-theme", (el) => el.innerHTML);
+
+
+    await page.waitForSelector('[name="username"]')
+    await page.type('[name="username"]', 'ebo')
+    await page.click('#get_free_button')
+    await page.waitForSelector('#count')
+    const text = await page.$eval("#count", (el) => el.innerHTML);
+
   res.send(text);
 
   await browser.close();
