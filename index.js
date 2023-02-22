c1262b35e5f722f0ec5c5bd5e337883a32e2b529
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 21:41:20 +0100

    fix

--- index.js
+++ index.js
@@ -23,6 +23,20 @@ app.post("/instagram", async (req, res) => {
   await browser.close();
 });
 
+app.post("/facebook", async (req, res) => {
+  const myText = req.body.facebook;
+  console.log(myText);
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto(myText);
+  await page.waitForTimeout(4000)
+  const text = await page.$eval("div.x9f619.x2lah0s.x1nhvcw1.x1qjc9v5.xozqiw3.x1q0g3np.x78zum5.x1iyjqo2.x1t2pt76.x1n2onr6.x1ja2u2z.x1h6rjhl > div.x9f619.x1n2onr6.x1ja2u2z.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.x78zum5.x1t2pt76 > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div > div > div:nth-child(1) > div > div:nth-child(3) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span", (el) => el.innerHTML);
+  res.send(text);
+  await browser.close();
+});
+
 app.post("/youtube", async (req, res) => {
   const myText = req.body.youtube;
   //console.log(myText);
