e2c47f274a9f7d023c63c788fa56d432fc9060b1
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 02:57:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", async (req, res) => {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/gio/");
+  await page.goto("https://www.instagram.com/");
   const text = await page.$eval("body", (el) => el.textContent);
   res.send(text);
 
