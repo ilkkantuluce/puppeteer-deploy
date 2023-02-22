094f673992547bd341711ef8e688aad5183d58ce
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-22 14:10:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const app = express();
 app.get("/", async (req, res) => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+    timeout: 100000
   });
   const page = await browser.newPage();
   await page.goto("https://livecounts.io/instagram-live-follower-counter/gio");
-  await page.waitForTimeout(4000)
-  const text = await page.$eval(".odometer.odometer-auto-theme", (el) => el.innerHTML);
+  await page.waitForTimeout(3000)
+  await page.screenshot({ path: 'fullpage.png', fullPage: true });
 
-
+  const text = await page.$eval("body", (el) => el.innerHTML);
   
   res.send(text);
 
