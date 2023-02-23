4a236b0ebd97f8991f22b9343456c7e97cb266cd
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-23 02:48:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.post("/instagram", async (req, res) => {
   const myText = req.body.instagram;
   console.log(myText);
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "--start-maximized"]
   });
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
   await page.goto(myText);
   await page.waitForTimeout(2000)
   const text = await page.$eval("body", (el) => el.innerHTML);
