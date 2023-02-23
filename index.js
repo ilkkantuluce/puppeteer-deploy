f502e33762d26b9bc7055ecb5ae33a601251fba6
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-23 01:47:44 +0100

    fix

--- index.js
+++ index.js
@@ -44,10 +44,15 @@ app.post("/facebook", async (req, res) => {
       const followers = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(4) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span:nth-child(1)", (el) => el.innerHTML);
       const totallikes = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(5) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span", (el) => el.innerHTML);
       res.send(followers + " " + totallikes);
+      let subCounts = await {
+        followers,
+        totallikes,
+      };
+      res.json(subCounts);
     } catch (error) {
       console.log("The element didn't appear.")
+      await browser.close();
     }
-
 });
 
 app.post("/youtube", async (req, res) => {
@@ -58,7 +63,6 @@ app.post("/youtube", async (req, res) => {
   });
   const page = await browser.newPage();
   await page.goto(myText);
-  await page.waitForTimeout(4000)
   const text = await page.$eval("#subscriber-count", (el) => el.innerHTML);
   res.send(text);
   await browser.close();
@@ -72,7 +76,6 @@ app.post("/tiktok", async (req, res) => {
   });
   const page = await browser.newPage();
   await page.goto(myText);
-  await page.waitForTimeout(4000)
   const following = await page.$eval("strong[data-e2e='following-count']", (el) => el.innerHTML);
   const followers = await page.$eval("strong[data-e2e='followers-count']", (el) => el.innerHTML);
   res.send(following + " " + followers);
