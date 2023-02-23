159e0185e6eb719a7514cd97d53da5faa0b4ca9f
ilkkantuluce <ilkkantuluce07@gamil.com>
2023-02-23 01:18:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,16 +46,14 @@ app.post("/facebook", async (req, res) => {
 
         await page.goto(myText);
         //await page.waitForTimeout(4000)
-        const followers = null;
-        const totallikes = null;
-        if(page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(3) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span:nth-child(1)", (el) => el.innerHTML) !== null) {
-          followers = page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(3) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span:nth-child(1)", (el) => el.innerHTML);
+        try {
+          const followers = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(3) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span:nth-child(1)", (el) => el.innerHTML);
           const totallikes = await page.$eval("div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6 > div.x6s0dn4.x78zum5.xdt5ytf.x193iq5w > div.x9f619.x193iq5w.x1talbiv.x1swvt13.x1pi30zi.x1y1aw1k > div > div.x9f619.x1n2onr6.x1ja2u2z.xeuugli.x1iyjqo2.xs83m0k.x1xmf6yo.x1emribx.x1e56ztr.x1i64zmx.xjl7jj.xnp8db0.x65f84u.x1xzczws > div.x7wzq59 > div:nth-child(1) > div > div > div > div.x1jx94hy.x78zum5.xdt5ytf > div:nth-child(4) > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.xamitd3.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn > div > div > span > span", (el) => el.innerHTML);
-        } else {
-          console.log("error");
+          res.send(followers + " " + totallikes);
+        } catch (error) {
+          console.log("The element didn't appear.")
         }
-        
-        res.send(followers + " " + totallikes);
+
         
 
     }catch (error) {
